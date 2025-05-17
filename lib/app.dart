280b9be930e4a87d91cd15e0af2f9b6a669698eb
LummyHills97 <akinnuliolumide97@gmail.com>
2025-05-17 18:59:21 +0100

    added the onboarding screen to my project, yet to input the skip options and others

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_store/app.dart';
+import 'package:ecommerce_store/features/authentication/screens/onboarding.dart';
 import 'package:ecommerce_store/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -9,19 +10,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'App', // Fixed here
+      title: 'App',
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Awesome! 🎊 Project Structure is set up and running. \n Happy T Coding 🎊',
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+      home: const OnboardingScreen(), // Set your onboarding screen as home
     );
   }
 }
--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce_store/utils/constants/image_strings.dart';
+import 'package:ecommerce_store/utils/constants/sizes.dart';
+import 'package:ecommerce_store/utils/constants/text_strings.dart';
+import 'package:ecommerce_store/utils/helpers/helpers_functions.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -5,6 +9,77 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Horizontal scrollable Pages
+          PageView(
+            children: const [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
+
+          // TODO: Skip Button
+
+          // TODO: Dot Navigation SmoothPageIndicator
+
+          // TODO: Circular Button
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            width: THelperFunctions.screenWidth() * 0.8,
+            height: THelperFunctions.screenHeight() * 0.6,
+            image: AssetImage(image),
+          ),
+          const SizedBox(height: TSizes.spaceBtwItems),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: TSizes.spaceBtwItems),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
