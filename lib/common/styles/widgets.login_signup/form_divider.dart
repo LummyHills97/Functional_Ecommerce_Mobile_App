c5a5778a9e6eb55e8c1a09433b7c0f7d61ed40b2
LummyHills97 <akinnuliolumide97@gmail.com>
2025-05-30 02:58:53 +0100

    worked on the signup screen and login page

---
+++ lib/common/styles/widgets.login_signup/form_divider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TFormDivider extends StatelessWidget {
+  const TFormDivider({super.key, required this.dividerText});
+  
+  final String dividerText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Flexible(
+          child: Divider(
+            color: Colors.grey,
+            thickness: 0.5,
+            indent: 60,
+            endIndent: 5,
+          ),
+        ),
+        Text(
+          dividerText,
+          style: Theme.of(context).textTheme.labelMedium?.copyWith(
+            color: Colors.grey[600],
+          ),
+        ),
+        const Flexible(
+          child: Divider(
+            color: Colors.grey,
+            thickness: 0.5,
+            indent: 5,
+            endIndent: 60,
+          ),
+        ),
+      ],
+    );
+  }
+}
