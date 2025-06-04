a5ff4b9291e38c3080af6ff4dfb08875cfd28377
LummyHills97 <akinnuliolumide97@gmail.com>
2025-06-04 21:01:50 +0100

    connect the app screen to the home page

--- lib/utils/constants/home.dart
+++ lib/utils/constants/home.dart
@@ -13,6 +13,19 @@ class HomeScreen extends StatelessWidget {
             Container(
               color: TColors.primary,
               padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  Container(
+                    width: 480,
+                    height: 480,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      color: TColors.textWhite.withOpacity(0.1),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
