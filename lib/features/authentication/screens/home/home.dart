19c9675dc6422b8dfcc7d7a158beab879cb8d95b
LummyHills97 <akinnuliolumide97@gmail.com>
2025-06-12 15:45:57 +0100

    added new files

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -6,85 +6,71 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-const HomeScreen({super.key});
-
-@override
-
-Widget build(BuildContext context) {
-
-return Scaffold(
-
-body: SingleChildScrollView(
-
-child: Column(
-
-children: [
-
-// header
-
-TPrimaryHeaderContainer(
-
-child: Column(
-
-children: [
-
-TAppBar(
-
-title: Column(
-
-crossAxisAlignment: CrossAxisAlignment.start,
-
-children: [
-
-Text(
-
-TTexts.homeAppbarTitle,
-
-style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)
-
- ),
-
-Text(
-
-TTexts.homeAppbarSubTitle,
-
-style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)
-
- ),
-
- ],
-
- ),
-
-actions: [
-
-IconButton(
-
-onPressed: () {},
-
-icon: const Icon(Iconsax.shopping_bag, color: TColors.white)
-
- )
-
- ],
-
- ),
-
- ],
-
- ),
-
- ),
-
- ],
-
- ),
-
- ),
-
- );
-
- }
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // header
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  TAppBar(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          TTexts.homeAppbarTitle,
+                          style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)
+                        ),
+                        Text(
+                          TTexts.homeAppbarSubTitle,
+                          style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      Stack(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Iconsax.shopping_bag, color: TColors.white)
+                          ),
+                          Positioned(
+                            right: 8,
+                            top: 8,
+                            child: Container(
+                              width: 18,
+                              height: 18,
+                              decoration: BoxDecoration(
+                                color: TColors.black,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  '2', // Badge number
+                                  style: TextStyle(
+                                    color: TColors.white,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
