6e1058b0478fb58ab2db4ff1cb537a3b3c42d227
LummyHills97 <akinnuliolumide97@gmail.com>
2025-06-15 13:38:49 +0100

    changed the home page

---
+++ lib/common/widgets/search_container.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:ecommerce_store/utils/constants/colors.dart';
+import 'package:ecommerce_store/utils/constants/sizes.dart';
+import 'package:ecommerce_store/utils/device/device_utility.dart';
+
+class TSearchContainer extends StatelessWidget {
+  const TSearchContainer({
+    super.key,
+    this.onTap,
+    this.text = 'Search in Store',
+  });
+
+  final VoidCallback? onTap;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: TDeviceUtils.getScreenWidth(context),
+        margin: EdgeInsets.all(TSizes.md),
+        padding: EdgeInsets.all(TSizes.md),
+        decoration: BoxDecoration(
+          color: isDark ? TColors.dark : TColors.light,
+          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+          border: Border.all(color: isDark ? TColors.darkerGrey : TColors.grey),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              Iconsax.search_normal,
+              color: isDark ? TColors.lightGrey : TColors.darkerGrey,
+              size: 20,
+            ),
+            SizedBox(width: TSizes.spaceBtwItems),
+            Text(
+              text,
+              style: TextStyle(
+                color: isDark ? TColors.lightGrey : TColors.darkerGrey,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,12 +1,8 @@
-import 'package:ecommerce_store/common/widgets/appbar.dart';
+import 'package:flutter/material.dart';
 import 'package:ecommerce_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:ecommerce_store/common/widgets/home_appbar.dart';
+import 'package:ecommerce_store/common/widgets/search_container.dart';
 import 'package:ecommerce_store/utils/constants/sizes.dart';
-import 'package:ecommerce_store/utils/constants/text_strings.dart';
-import 'package:ecommerce_store/utils/constants/colors.dart';
-import 'package:ecommerce_store/utils/device/device_utility.dart';
-import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,53 +13,39 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // header
+            /// Header Section
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar(),
+                  const THomeAppBar(),
                   
-                  // Search Bar
-                  Container(
-                    width: TDeviceUtils.getScreenWidth(context),
-                    margin: EdgeInsets.all(TSizes.md),
-                    padding: EdgeInsets.all(TSizes.md),
-                    decoration: BoxDecoration(
-                      color: TColors.white,
-                      borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-                      border: Border.all(color: TColors.grey),
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Iconsax.search_normal,
-                          color: TColors.darkerGrey,
-                          size: 20,
+                  /// Search Bar with navigation
+                  TSearchContainer(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const TSearchContainer(),
                         ),
-                        SizedBox(width: TSizes.spaceBtwItems),
-                        Text(
-                          'Search in Store',
-                          style: TextStyle(
-                            color: TColors.darkerGrey,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                   
-                  SizedBox(height: TSizes.spaceBtwSections),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             ),
             
-            // Body content
+            /// Body Content
             Padding(
-              padding: EdgeInsets.all(TSizes.defaultSpace),
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
               child: Column(
                 children: [
-                  // Add your body content here
-                  Text('Body Content Goes Here'),
+                  Text(
+                    'Body Content Goes Here',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  // Add additional widgets here
                 ],
               ),
             ),
