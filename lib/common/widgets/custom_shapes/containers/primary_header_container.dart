1652f19ef0ad8f9496305c99eaea92ea30cb41b1
LummyHills97 <akinnuliolumide97@gmail.com>
2025-06-15 20:41:59 +0100

    added the categories section

--- lib/common/widgets/custom_shapes/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_store/common/widgets/custom_shapes/circular_container.dart';
+import 'package:ecommerce_store/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:ecommerce_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:ecommerce_store/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
