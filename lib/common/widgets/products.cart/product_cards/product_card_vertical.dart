2d1a2ed199818141b0caca4d2ea0910dc60b08bb
LummyHills97 <akinnuliolumide97@gmail.com>
2025-06-28 04:59:48 +0100

    added the shadow style and the product card

---
+++ lib/common/widgets/products.cart/product_cards/product_card_vertical.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TProductCardVertical extends StatelessWidget {
+  const TProductCardVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Container with side paddings, color, edge, radius and shadow
+    return const Placeholder();
+  }
+}
