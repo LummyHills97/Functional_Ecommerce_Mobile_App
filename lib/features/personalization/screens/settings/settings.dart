52e14b9a2dd75b48381816daad59d2674ca1a916
LummyHills97 <akinnuliolumide97@gmail.com>
2025-08-10 06:37:27 +0100

    wokring on the settings page

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,27 +1,41 @@
-
-
-import 'package:ecommerce_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:flutter/material.dart';
+import 'package:ecommerce_store/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:ecommerce_store/utils/constants/colors.dart';
+import 'package:ecommerce_store/utils/constants/sizes.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final headlineStyle = Theme.of(context)
+        .textTheme
+        .headlineMedium
+        ?.apply(color: TColors.white);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // Header
-            TPrimaryHeaderContainer(child: Column(
-               children: [
-                Text('ACCOUNT')
-               ],
-            )),
+            // Header Section
+            TPrimaryHeaderContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TAppBar(
+                    title: Text(
+                      'ACCOUNT',
+                      style: headlineStyle,
+                    ),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                ],
+              ),
+            ),
           ],
         ),
-      )
-
+      ),
     );
   }
 }
