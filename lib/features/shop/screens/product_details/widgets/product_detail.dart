2a53d9507ac92bb524e146510bf038704707ba95
LummyHills97 <akinnuliolumide97@gmail.com>
2025-08-24 13:54:11 +0100

    working on the product detail

---
+++ lib/features/shop/screens/product_details/widgets/product_detail.dart
@@ -0,0 +1,38 @@
+
+import 'package:ecommerce_store/common/widgets/curved_edges_widget.dart';
+import 'package:ecommerce_store/utils/constants/colors.dart';
+import 'package:ecommerce_store/utils/constants/image_strings.dart';
+import 'package:ecommerce_store/utils/helpers/helpers_functions.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Product Image Slider
+             TCurvedEdgeWidget(
+              child: Container(
+                color: dark ? TColors.darkerGrey : TColors.light,
+                child: Stack(
+                  children: [
+                    // Main large image
+                    Image(image: AssetImage(TImages.productImage1)),
+                  ],
+                ),
+              ),
+              )
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
