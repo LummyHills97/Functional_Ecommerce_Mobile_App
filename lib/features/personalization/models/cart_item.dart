8207cefa810f0875ae9c6d0dbeab94ddd9b9b283
LummyHills97 <akinnuliolumide97@gmail.com>
2025-09-27 20:59:54 +0100

    chnaged the whole cart_item

--- lib/features/personalization/models/cart_item.dart
+++ lib/features/personalization/models/cart_item.dart
@@ -2,7 +2,7 @@ class CartItem {
   final String id;
   final String name;
   final String image;
-  final String brand;   // 👈 new
+  final String brand;
   final String size;
   final String color;
   final double price;
@@ -12,7 +12,7 @@ class CartItem {
     required this.id,
     required this.name,
     required this.image,
-    required this.brand, // 👈 new
+    this.brand = "Default", // 👈 default to avoid errors
     required this.size,
     required this.color,
     required this.price,
@@ -23,7 +23,7 @@ class CartItem {
     String? id,
     String? name,
     String? image,
-    String? brand, // 👈 new
+    String? brand,
     String? size,
     String? color,
     double? price,
@@ -33,7 +33,7 @@ class CartItem {
       id: id ?? this.id,
       name: name ?? this.name,
       image: image ?? this.image,
-      brand: brand ?? this.brand, // 👈 new
+      brand: brand ?? this.brand,
       size: size ?? this.size,
       color: color ?? this.color,
       price: price ?? this.price,
