ddf36857bc96cfe0769046fba7a55a06fce24550
LummyHills97 <akinnuliolumide97@gmail.com>
2025-10-04 23:01:01 +0100

    created new pages which are order page and order list page

---
+++ lib/features/authentication/screens/order/widgets/order.dart
@@ -0,0 +1,23 @@
+
+import 'package:ecommerce_store/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce_store/features/authentication/screens/order/widgets/order_list.dart';
+import 'package:ecommerce_store/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBar
+      appBar: TAppBar(title: Text ('My Orders', style: Theme.of(context). textTheme.headlineSmall), showBackArrow: true,),
+      body: const Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+
+        // orders
+        child: TOrderListItems(),
+      )
+    );
+  }
+}
